1bd50cad2094102880c0e133298a65682bceb308
IzKirill <izmailov.ka@phystech.edu>
2023-10-29 22:38:46 +0300

    add ListCtor and ListDtor

--- List.cpp
+++ List.cpp
@@ -1,27 +1,82 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <assert.h>
 
+FILE* LogFile = 0;
 
+ListErrors ListCtor(List* List, size_t Capacity, const char* list_name,
+				    const size_t birth_line, const char* birth_file, const char* birth_function) 
+{    
+	assert(List != nullptr);
+	assert(Capacity > 0);
 
+	LogFile = fopen(NameLogFile, "w");
+	
+	List->IsCtor = true;
+	List->IsDtor = false;
+	
+	List->Info.list_name = list_name;
+	List->Info.birth_function = birth_function;
+	List->Info.birth_line = birth_line;
+	List->Info.birth_file = birth_file;
 
+	List->Capacity = Capacity + 2;
 
+	List->Data = (Elemt*) calloc(List->Capacity; sizeof(Elemt));
+	assert(List->Data != nullptr);
 
+	List->NextElement = (int*) calloc(List->Capacity; sizeof(int));
+	assert(List->NextElement != nullptr);
 
+	List->PrevElement = (int*) calloc(List->Capacity; sizeof(int));
+	assert(List->PrevElement != nullptr);
 
+	for (size_t NumberElement = 0; NumberElement < List->Capacity; NumberElement++) {
+		List->Data[NumberElement] = PoisonValue;
+		List->NextElement[NumberElement] = NumberElement + 1;
+		List->PrevElement[NumberElement] = PoisonValue;
+	}
 
+	List->Head = 0;	
+	List->Tail = 1;
+	List->NextElement[List->Head] = List->Tail;
+	List->PrevElement[List->Head] = -2;
+	List->NextElement[List->Tail] = -2;
+	List->PrevElement[List->Tail] = List->Head;
 
+	List->FreeElement = 2;
+	List->NumberElements = 0;
 
+	List->ErrorsInfo = 0;
 
+	if (ListOK(List)) return ERROR;
+}
 
+ListErrors ListDtor(List* List)
+{
+	assert(List != nullptr);
+	assert(IsCtor == true);
+	assert(IsDtor == false);
 
+	if (ListOK(List)) return ERROR;
 
+	List->IsDtor = true;
 
+	for (size_t NumberElement = 0; NumberElement < List->Capacity; NumberElement++) {
+		List->Data[NumberElement] = PoisonValue;
+		List->NextElement[NumberElement] = PoisonValue;
+		List->PrevElement[NumberElement] = PoisonValue;
+	}
+	free(List->Data);
+	free(List->NextElement);
+	free(List->PrevElement);
+	List->Capacity = PoisonValue;
+	List->FreeElement = PoisonValue;
+	List->Head = PoisonValue;
+	List->Tail = PoisonValue;
+	List->NumberElements = PoisonValue;
 
-
-
-
-
-
-
-
+}
 
 
 
--- List.h
+++ List.h
@@ -1,41 +1,48 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
-const char LogFileName = "ListLogs.txt";
+#define LIST_CTOR(List, Capacity) ListCtor((List), (Capacity), #List, \
+                                  __LINE__,__FILE__,__PRETTY_FUNCTION__)
 
-typedef int Elemt
+const char NameLogFile = "ListLogs.txt";
+
+const int PoisonValue = 2399229923;
+
+typedef int Elemt;
 
 struct BirthInfo {
-    const char* ListName;  
-    const char* BirthFunction;
-    size_t BirthLine;
-    const char* BirthFile;
+    const char* list_name;  
+    const char* birth_function;
+    size_t birth_line;
+    const char* birth_file;
 }; 
 
 struct List {
 	Elemt* Data;
-	Elemt* NextElement;
-	Elemt* PrevElement;
+	int* NextElement;
+	int* PrevElement;
 	size_t Head;
 	size_t Tail;
 	size_t NumberElements;
 	size_t Capacite;
+	size_t Tail;                       
+	size_t Capacity;
 	size_t FreeElement;
+	char ErrorsInfo;
 	bool IsCtor;
 	bool IsDtor;
-	BirthInfo ListInfo;	
+	BirthInfo Info;	
 };
 
 enum ListErrors {
-	LIST_OK = 0
+	LIST_OK = 0,
+	LIST_ERROR = -1
 };
 
-ListErrors ListCtor();
-ListErrors ListDtor();
-ListErrors ListOK();
-ListErrors ListDump();
-
-
-
+ListErrors ListCtor(List* List, size_t Capacity, const char* list_name,
+               	    const size_t birth_line, const char* birth_file, const char* birth_function);
+ListErrors ListDtor(List* List);
+ListErrors ListOK(List* List);
+ListErrors ListDump(List* List);
 
 #endif
